3f0379c3ed4667aeb0d6725525b75b39efb5b370
prashantkumar999 <prashant047alg@gmail.com>
2025-04-13 16:00:04 +0530

    create subsection controller added

---
+++ server/controllers/SubSection.js
@@ -0,0 +1,60 @@
+import SubSection from "../models/SubSection"
+import Section from "../models/Section"
+import { uploadMediaToCloudinary } from "../utils/imageUploader"
+
+//create subsection
+export const createSubSection = async (req, res) => {
+    try {
+        //get data
+        const { title, timeDuration, description, sectionId } = req.body
+        //get video 
+        const video = req.files.videoFile
+        //video file check 
+        if (!video) {
+            return res.status(400).json({
+                success: false,
+                message: "video file is required"
+            })
+        }
+        //data validation 
+        if (!title || !timeDuration || !description || !sectionId) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+        // upload video to cloudinary to get the url 
+        const uploadedVideoDetails = await uploadMediaToCloudinary(video, process.env.FOLDER_NAME)
+        // create subsection
+        const newSubSection = await SubSection.create({
+            title,
+            timeDuration,
+            description,
+            videoUrl: uploadedVideoDetails.secure_url
+        })
+        // console.log(newSubSection)
+        // add subsection to section 
+
+        const updatedSection = await Section.findByIdAndUpdate(sectionId, {
+            $push: {
+                subSection: newSubSection._id
+            }
+        }, { new: true }).populate("subSection").exec()
+        //return response
+        return res.status(200).json({
+            success: true,
+            message: "sub-section created successfully",
+            data: updatedSection
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "something went wrong!",
+        })
+    }
+}
+
+//update subSection
+
+
+//delete subSection
--- server/utils/imageUploader.js
+++ server/utils/imageUploader.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary';
 
-export const uploadImageToCloudinary = async (file, folder, height, quality) => {
+export const uploadMediaToCloudinary = async (file, folder, height, quality) => {
     const options = { folder }
     if (height) {
         options.height = height
